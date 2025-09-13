daa5f5c98700aa1f6d1f33a57635c5d730569a35
PBX_g33k <development@oguzhanuysal.eu>
2025-09-13 11:02:31 +0200

    OPT: Add getIdentifier to DownloadInterface

--- api/src/Service/Downloader/DownloaderInterface.php
+++ api/src/Service/Downloader/DownloaderInterface.php
@@ -8,6 +8,11 @@ use Psr\Http\Message\UriInterface;
 interface DownloaderInterface
 {
     /**
+     * Get the unique identifier of this downloader.
+     */
+    public function getIdentifier(): string;
+
+    /**
      * Download the given URI using the downloader service.
      * Only return true if the download request was successfully sent to the downloader service.
      * In any other case, throw an exception, which should be handled by the caller.
