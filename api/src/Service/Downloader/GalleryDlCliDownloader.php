cb62fa0155b1a7ba50b3608b695112308df7837f
PBX_g33k <development@oguzhanuysal.eu>
2025-09-13 12:02:06 +0200

    NEW: Create default gallery-dl config file if it doesn't exist

--- api/src/Service/Downloader/GalleryDlCliDownloader.php
+++ api/src/Service/Downloader/GalleryDlCliDownloader.php
@@ -5,6 +5,7 @@ namespace App\Service\Downloader;
 use App\Enum\DownloaderTypeEnum;
 use Psr\Cache\CacheItemPoolInterface;
 use Psr\Http\Message\UriInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\Process\Process;
 use Symfony\Contracts\Cache\ItemInterface;
@@ -17,14 +18,28 @@ class GalleryDlCliDownloader implements DownloaderInterface
         #[Autowire(param: 'downloader.gallery_dl_cli.config_path')]
         private(set) string $configPath,
         #[Autowire(param: 'downloader.gallery_dl_cli.binary_path')]
-        private(set) string $binaryPath
+        private(set) string $binaryPath,
+        private LoggerInterface $logger
     )
     {
     }
 
     public function download(UriInterface $uri): true
     {
+        $this->createConfigFileIfNotExists();
+
         // TODO: Implement download() method.
+        $downloadProcess = new Process([
+            $this->binaryPath,
+            '--config', $this->configPath,
+            $uri->__toString()
+        ]);
+        $downloadProcess->mustRun();
+        if (!$downloadProcess->isSuccessful()) {
+            throw new \RuntimeException($downloadProcess->getErrorOutput());
+        } else {
+            return true;
+        }
     }
 
     public function getDownloaderType(): DownloaderTypeEnum
@@ -63,6 +78,43 @@ class GalleryDlCliDownloader implements DownloaderInterface
     {
         return 'gallery-dl-cli';
     }
+
+    private function createConfigFileIfNotExists(): void
+    {
+        if (!file_exists($this->configPath)) {
+            // Run the command to create a default config file.
+            // gallery-dl --config-create
+            // Since gallery-dl only writes the file to /etc/gallery-dl.conf by default,
+            // we need to move it to the desired location.
+
+            $process = new Process([$this->binaryPath, '--config-create']);
+            $process->mustRun();
+            if (!$process->isSuccessful()) {
+                throw new \RuntimeException($process->getErrorOutput());
+            }
+            // Get the output file path from the command output.
+            // Example output: "[config][info] Created a basic configuration file at '/etc/gallery-dl.conf'"
+            $output = $process->getErrorOutput();
+            preg_match("/'(.+?)'/", $output, $matches);
+            if (isset($matches[1]) && file_exists($matches[1])) {
+                // Make sure the target directory exists
+                $targetDir = dirname($this->configPath);
+                if (!is_dir($targetDir)) {
+                    mkdir($targetDir, 0755, true);
+                }
+
+                rename($matches[1], $this->configPath);
+            } else {
+                $this->logger->error('Failed to find the created gallery-dl config file.',[
+                    'error_output' => $process->getErrorOutput(),
+                    'output' => $output,
+                    'matches' => $matches,
+                ]);
+                throw new \RuntimeException('Failed to create gallery-dl config file.');
+            }
+        }
+    }
+
 
     private function fetchSupportedDomains(): array
     {
