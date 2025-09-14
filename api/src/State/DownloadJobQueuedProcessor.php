8d4bd7b6549e6186c1f1de04978613ea0006a391
PBX_g33k <development@oguzhanuysal.eu>
2025-09-14 19:33:21 +0200

    NEW: Cache (succesfull) domain probes for 24 hours
    probing yt-dlp takes a few seconds.
    Local (dev-mode) testing improved ingest time from 14434 ms to 52 ms

--- api/src/State/DownloadJobQueuedProcessor.php
+++ api/src/State/DownloadJobQueuedProcessor.php
@@ -10,9 +10,12 @@ use App\Entity\DownloadJob;
 use App\Enum\DownloadStateEnum;
 use App\Factory\DownloaderFactory;
 use GuzzleHttp\Psr7\Uri;
+use Psr\Cache\CacheItemPoolInterface;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\Exception\BadRequestException;
 use ApiPlatform\Symfony\Messenger\Processor as MessengerProcessor;
+use Symfony\Contracts\Cache\ItemInterface;
+use Symfony\Contracts\Cache\TagAwareCacheInterface;
 
 
 class DownloadJobQueuedProcessor implements ProcessorInterface
@@ -24,7 +27,8 @@ class DownloadJobQueuedProcessor implements ProcessorInterface
         #[Autowire(service: 'api_platform.doctrine.orm.state.persist_processor')]
         private ProcessorInterface $persistProcessor,
         private MessengerProcessor $messengerProcessor,
-        private DownloaderFactory $downloaderFactory
+        private DownloaderFactory $downloaderFactory,
+        private TagAwareCacheInterface $cache
     )
     {
     }
@@ -55,12 +59,34 @@ class DownloadJobQueuedProcessor implements ProcessorInterface
         if (isset($data->downloader)) {
             $downloadJob->setDownloader($data->downloader);
         } else {
-            // Try to determine the downloader by URI
-            // This will for example run "yt-dlp --simulate <uri>" to see if yt-dlp supports the given URI
-            $downloaders = $this->downloaderFactory->getDownloadersByUri(new Uri($data->uri));
-            foreach ($downloaders as $downloader) {
-                $downloadJob->setDownloader($downloader->getIdentifier());
-                break; // Use the first matching downloader
+
+            $downloaderKey = $this->cache->get($this->getDomainProbeCacheKey($downloadJob), function (ItemInterface $item) use ($downloadJob) {
+                $item->expiresAfter(3600); // Cache for 1 hour
+
+                $item->tag([
+                    'dlsupport', // Global tag for all download support checks
+                    // Tag for the specific domain, so we can invalidate it if needed
+                    $this->getDomainProbeCacheKey($downloadJob)
+                ]);
+                // Try to determine the downloader by URI
+                // This will for example run "yt-dlp --simulate <uri>" to see if yt-dlp supports the given URI
+                $downloaders = $this->downloaderFactory->getDownloadersByUri(new Uri($downloadJob->getUrl()));
+                foreach ($downloaders as $downloader) {
+                    // Cache for 24 hours if a downloader was found
+                    $item->expiresAfter(86400);
+                    return $downloader->getIdentifier();
+//                    $downloadJob->setDownloader($downloader->getIdentifier());
+                    break; // Use the first matching downloader
+                }
+
+                return false; // No downloader found
+            });
+            if ($downloaderKey) {
+                $downloadJob->setDownloader($downloaderKey);
+            } else {
+                // Invalidate the cache for this domain, so we can try again next time
+                // This is useful if a site was not supported before, but is supported now
+                $this->cache->invalidateTags([$this->getDomainProbeCacheKey($downloadJob)]);
             }
 
             if (!$downloadJob->getDownloader()) {
@@ -83,5 +109,12 @@ class DownloadJobQueuedProcessor implements ProcessorInterface
 
         // Dispatch the job to the messenger queue
         return $this->messengerProcessor->process($downloadJob,$operation,$uriVariables,$context);
+    }
+
+    private function getDomainProbeCacheKey(DownloadJob $downloadJob): string
+    {
+        // Use the domain as the cache key
+        // ie: dlsupport_example.com
+        return 'dlsupport_' . $downloadJob->getUrl()->getHost();
     }
 }
