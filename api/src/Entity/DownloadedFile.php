dc2556d94caf7e8811dffa824f3f5a8dee1fbd12
PBX_g33k <development@oguzhanuysal.eu>
2025-09-18 23:57:06 +0200

    NEW: DownloadedFile entity

---
+++ api/src/Entity/DownloadedFile.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\DownloadedFileRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: DownloadedFileRepository::class)]
+class DownloadedFile
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    /**
+     * @var Collection<int, DownloadJob>
+     */
+    #[ORM\ManyToMany(targetEntity: DownloadJob::class, inversedBy: 'downloadedFiles')]
+    private Collection $downloadJob;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $path = null;
+
+    #[ORM\Column]
+    private array $metadata = [];
+
+    #[ORM\Column]
+    private ?bool $visible = null;
+
+    public function __construct()
+    {
+        $this->downloadJob = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return Collection<int, DownloadJob>
+     */
+    public function getDownloadJob(): Collection
+    {
+        return $this->downloadJob;
+    }
+
+    public function addDownloadJob(DownloadJob $downloadJob): static
+    {
+        if (!$this->downloadJob->contains($downloadJob)) {
+            $this->downloadJob->add($downloadJob);
+        }
+
+        return $this;
+    }
+
+    public function removeDownloadJob(DownloadJob $downloadJob): static
+    {
+        $this->downloadJob->removeElement($downloadJob);
+
+        return $this;
+    }
+
+    public function getPath(): ?string
+    {
+        return $this->path;
+    }
+
+    public function setPath(string $path): static
+    {
+        $this->path = $path;
+
+        return $this;
+    }
+
+    public function getMetadata(): array
+    {
+        return $this->metadata;
+    }
+
+    public function setMetadata(array $metadata): static
+    {
+        $this->metadata = $metadata;
+
+        return $this;
+    }
+
+    public function isVisible(): ?bool
+    {
+        return $this->visible;
+    }
+
+    public function setVisible(bool $visible): static
+    {
+        $this->visible = $visible;
+
+        return $this;
+    }
+}
