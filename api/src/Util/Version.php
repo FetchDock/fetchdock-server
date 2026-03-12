48c874e4546dfc8c36b3893975571b05ccb2a186
PBX_g33k <development@oguzhanuysal.eu>
2026-03-12 01:13:35 +0100

    update VersionController to return the APP_VERSION environment variable

--- api/src/Util/Version.php
+++ api/src/Util/Version.php
@@ -6,10 +6,7 @@ class Version
 {
     public static function getVersion(): string
     {
-        // Look for the VERSION file which is generated during RELEASE
-        // If the file does not exist, we're probably running from source
-        return file_exists(__DIR__.'/../../VERSION')
-            ? file_get_contents(__DIR__.'/../../VERSION')
-            : 'dev';
+        // return the APP_VERSION environment variable
+        return $_ENV['APP_VERSION'] ?? 'dev';
     }
 }
