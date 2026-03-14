131cd37039da0efc45a2ce6a1aec8cd771c6e68c
PBX_g33k <development@oguzhanuysal.eu>
2026-03-14 09:08:19 +0100

    Fix build failure

--- pwa/types.ts
+++ pwa/types.ts
@@ -6,7 +6,7 @@ import {AuthProvider, AuthRedirectResult, QueryFunctionContext, UserIdentity} fr
 
 type RefreshableAuthProvider = {
   canRefresh?: (params: any & QueryFunctionContext) => Promise<boolean>;
-  refreshToken?: (params: any & QueryFunctionContext) => Promise<void>;
+  refreshToken?: () => Promise<void>;
 }
 
 export type OAuthProvider = AuthProvider & RefreshableAuthProvider;
