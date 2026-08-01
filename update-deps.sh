e6c6dd258cf441774126f6e45b94d4c2c234f763
PBX_g33k <development@oguzhanuysal.eu>
2026-08-01 10:46:51 +0200

    OPT: update-deps.sh now makes use of make commands

--- update-deps.sh
+++ update-deps.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
 # Remove all running containers
-docker compose down -v
+make down
+#docker compose down -v
 
 # Update Docker images
-docker compose build --no-cache --pull
+make build
+#docker compose build --no-cache --pull
 
 # Update deps
-docker compose run php /bin/sh -c 'composer update; composer outdated'
-docker compose run pwa /bin/sh -c 'pnpm install; pnpm update; pnpm outdated'
+make api "/bin/sh -c 'composer update; composer outdated'"
+make pwa "/bin/sh -c 'pnpm install; pnpm update; pnpm outdated'"
 
 # Update Symfony recipes
 cd api
@@ -16,4 +18,4 @@ composer recipes:update
 
 echo 'Run `git diff` and carefully inspect the changes made by the recipes.'
 echo 'Run `docker compose up --wait --force-recreate` now and check that everything is fine!'
-echo 'Run `docker compose exec php /bin/sh -c ''bin/console -e test doctrine:database:create ; bin/console -e test doctrine:migrations:migrate --no-interaction ; bin/phpunit ; bin/console -e test doctrine:schema:validate''` to check that the tests are green.'
+echo 'Run `docker compose exec api /bin/sh -c ''bin/console -e test doctrine:database:create ; bin/console -e test doctrine:migrations:migrate --no-interaction ; bin/phpunit ; bin/console -e test doctrine:schema:validate''` to check that the tests are green.'
